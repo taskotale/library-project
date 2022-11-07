3132faf96d046bea006e59d19d00373ff6d3ecb6
taskotale <110062804+taskotale@users.noreply.github.com>
2022-11-07 12:19:57 -0500

    printing on page options

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 let myLibrary = [];
+const bookshelf = document.getElementById('bookshelf')
+
+const showLibrary = () => {
+    myLibrary.forEach(book => {
+        let bookCard = document.createElement('div')
+        bookCard.innerText = (book.info())
+        bookshelf.appendChild(bookCard)
+    });
+}
+
+const addOnBookshelf = (addedBook) => {
+    let bookCard = document.createElement('div')
+        bookCard.innerText = (addedBook.info())
+        bookshelf.appendChild(bookCard)
+}
+
+const book1 = new Book('addTitle', 'addAuthor', 'addPages', 'addRead')
+myLibrary.push(book1)
+const book2 = new Book('bookrandom', 'someone', '23423', 'no')
+myLibrary.push(book2)
+const book3 = new Book('dsada', 'zdc', '213', 'no')
+myLibrary.push(book3)
+const book4 = new Book('fsdf', 'addAudfdsfthor', '333', '1')
+myLibrary.push(book4)
+
+showLibrary()
 
 const addBookBtn = document.getElementById('add-new-book-btn')
 const addNewBook = document.getElementById('add-new-book')
-addBookBtn.addEventListener('click', e=>addNewBook.classList.add('active'))
+addBookBtn.addEventListener('click', e => addNewBook.classList.add('active'))
 
+// make this neater, without too many get element
 const bookTitle = document.getElementById('book-title')
 const bookAuthor = document.getElementById('book-author')
 const bookPages = document.getElementById('book-pages')
@@ -11,10 +38,16 @@ const bookRead = document.getElementById('book-read')
 
 const submitBtn = document.getElementById('submit-btn')
 
-let inputTitle
-let inputAuthor
-let inputPages
-let inputRead
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function () {
+        //change the info message
+        return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
+    }
+}
 
 const getBookDetails = (event) => {
     inputTitle = bookTitle.value
@@ -22,25 +55,14 @@ const getBookDetails = (event) => {
     inputPages = bookPages.value
     inputRead = bookRead.value
     addBookToLibrary(inputTitle, inputAuthor, inputPages, inputRead)
-    event.preventDefault();
+    event.preventDefault(); //just for know to stop browser from refreshing
     addNewBook.classList.remove('active')
+    addOnBookshelf(myLibrary[myLibrary.length-1])
 }
 
 submitBtn.addEventListener('click', getBookDetails)
-
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function () {
-        return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
-    }
-}
 
 function addBookToLibrary(addTitle, addAuthor, addPages, addRead) {
     addTitle = new Book(addTitle, addAuthor, addPages, addRead)
     myLibrary.push(addTitle)
 }
-
-
