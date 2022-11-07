8c1fc260a1d28fae284db80bda4b160915095036
taskotale <110062804+taskotale@users.noreply.github.com>
2022-11-07 11:12:20 -0500

    basic book input done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let myLibrary = [];
 
+const addBookBtn = document.getElementById('add-new-book-btn')
+const addNewBook = document.getElementById('add-new-book')
+addBookBtn.addEventListener('click', e=>addNewBook.classList.add('active'))
+
 const bookTitle = document.getElementById('book-title')
 const bookAuthor = document.getElementById('book-author')
 const bookPages = document.getElementById('book-pages')
@@ -12,12 +16,14 @@ let inputAuthor
 let inputPages
 let inputRead
 
-const getBookDetails = () => {
+const getBookDetails = (event) => {
     inputTitle = bookTitle.value
     inputAuthor = bookAuthor.value
     inputPages = bookPages.value
     inputRead = bookRead.value
-    addBookToLibrary (inputTitle, inputAuthor, inputPages, inputRead)
+    addBookToLibrary(inputTitle, inputAuthor, inputPages, inputRead)
+    event.preventDefault();
+    addNewBook.classList.remove('active')
 }
 
 submitBtn.addEventListener('click', getBookDetails)
@@ -33,7 +39,8 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(addTitle, addAuthor, addPages, addRead) {
-    const addBook = new Book (addTitle, addAuthor, addPages, addRead)
-    myLibrary.push(addBook)
+    addTitle = new Book(addTitle, addAuthor, addPages, addRead)
+    myLibrary.push(addTitle)
 }
+
 
